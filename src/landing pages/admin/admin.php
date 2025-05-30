37a10bb9df9ad39f80180869877a2b4f2aceb89b
AARB2006 <augustusantoine2006@gmail.com>
2025-05-30 16:11:36 +0800

    FINALLY OH MY GOD

--- src/landing pages/admin/admin.php
+++ src/landing pages/admin/admin.php
@@ -21,15 +21,30 @@
 
     
 
-    <div>
-        <div>
-            <h1>Add User</h1>
+    <div
+        class = "bg-[#f3f8f1] shadow-md sticky top-0 w-full p-4 flex items-center justify-between"
+    >
+        <div class="flex items-center space-x-10">
+
+            <img src="../../../imgs/menu-hamburger-navigation-512-4184062329.png" alt="" class="h-6 w-auto object-scale-down pl-4">
+            <a href="#" class="flex items-center space-x-2">
+            <img src="../../../imgs/cc.png" alt="" class="h-10 w-auto object-scale-down">
+            <span class="text-lg font-semibold text-gray-800">Learning Management System (Admin's Side)</span>
+            </a>
 
         </div>
-        <div>
-            <h1>Add Course</h1>
-        </div>
+        
+        <nav>
+            <ul class = "flex space-x-4">
+                <li>
+                    <h2 class = "text-black pr-7 hover:underline hover:text-gray-400"><?php echo $displayName; ?>'s Account</h2>
+                </li>
+            </ul>
+        </nav>
+        
     </div>
+
+
 
 </body>
 </html>
